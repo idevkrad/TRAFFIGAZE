b29688027f9530f1fd6446aa127deb0874664b10
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-02-20 00:19:12 +0800

    312

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -18,7 +18,7 @@ export default {
 
     methods : {
         listenForNewEvent(){
-            Echo.join('presence-post')
+            Echo.join('presence-posts')
                 .listen('PostBroadcast', (data) => {
                     console.log(data.event);
                 });

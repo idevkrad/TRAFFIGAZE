759251f3246731b687f1faff8dc5517896cfc940
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-02-20 01:33:01 +0800

    451

--- public/js/app.js
+++ public/js/app.js
@@ -133,7 +133,7 @@ __webpack_require__.r(__webpack_exports__);
   },
   methods: {
     listenForNewEvent: function listenForNewEvent() {
-      Echo.join('postgjhs').listen('PostBroadcast', function (data) {
+      Echo.join('presence-posts').listen('PostBroadcast', function (data) {
         console.log(data.event);
       });
     }

e762890862c69b3bdee15fbdde5112d90ab0665f
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-02-20 02:25:00 +0800

    2231

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,15 +36,19 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com'),
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
+                'host' =>  '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'https',
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
+            'curl_options' => [
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+            ]
         ],
         
 

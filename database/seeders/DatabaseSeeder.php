fb772260f5295c5d13f0ee70a2f890e9e038057c
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-02-19 11:57:17 +0800

    231

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,6 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
+        $this->call(TagsTableSeeder::class);
     }
 }
---
+++ database/seeders/TagsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('tags')->delete();
+        
+        \DB::table('tags')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Traffic',
+                'icon' => 'marker.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
+        ));
+        
+        
+    }
+}

dab92a86c526c6cdb77c55b50fb6eb05de0d2c54
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-03-17 01:34:29 +0800

    Update PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -60,8 +60,24 @@ class PostController extends Controller
                 }
 
                 $coordinates = $request->coordinates;
-                
-                $imageName = 'wewe';
+                if($request->image){
+                    $dd = $request->image;
+                    $img = explode(',', $dd);
+                    $ini =substr($img[0], 11);
+                    $type = explode(';', $ini);
+                    if($type[0] == 'png'){
+                        $image = str_replace('data:image/png;base64,', '', $dd);
+                    }else{
+                        $image = str_replace('data:image/jpeg;base64,', '', $dd);
+                    }
+                    $image = str_replace(' ', '+', $image);
+                    $imageName =  date('Y').'-'.date('mhis').'.'.$type[0];
+                    
+                    if(\File::put(public_path('images/posts'). '/' . $imageName, base64_decode($image))){
+                        
+                    }
+                }
+                $image = 'wewe';
                 $data = Post::create(array_merge($request->all(),['coordinates' => json_encode($coordinates), 'image' => $imageName]));
                 return $data;
             });

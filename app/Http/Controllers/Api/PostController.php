b8606b95279ae6eb1982d13a818e412e626152b0
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-03-17 01:00:45 +0800

    Update PostController.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
     public function index(Request $request){
 
         if($request->type == 'latest'){
-            $data = Post::whereDate('created_at',now())->get();
+            $data = Post::whereDate('created_at',now())->ordeyBy('created_at','DESC')->get();
         }else if($request->type == 'all'){
             $data = Post::whereDate('created_at',now())->get();
         }else{

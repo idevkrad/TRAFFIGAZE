dffb3e6897c7e89919caee19194d4f135efa4cd5
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-03-17 11:10:20 +0800

    213

--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -27,6 +27,46 @@ class TagsTableSeeder extends Seeder
                 'created_at' => '2023-02-19 10:55:26',
                 'updated_at' => '2023-02-19 10:55:26',
             ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Road Block',
+                'icon' => 'roadBlocked.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Flooded Road',
+                'icon' => 'floodedRoad.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'Road Accident',
+                'icon' => 'roadAccident.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'One Way',
+                'icon' => 'oneWay.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'name' => 'Dangerous Road',
+                'icon' => 'dangerousRoad.png',
+                'created_at' => '2023-02-19 10:55:26',
+                'updated_at' => '2023-02-19 10:55:26',
+            ),
         ));
         
         

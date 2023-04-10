eb4b999edd21457b56d5473578248e77d3b98341
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-04-10 16:21:06 +0800

    we

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -71,7 +71,7 @@ class StatsController extends Controller
                 }
             ]);
         }
-        $tags = $query->orderBy('posts_count', 'desc')->get();
+        $tags = $query->orderBy('posts_count', 'desc')->LIMIT(10)->get();
         
         if($request->type == 'yesterday'){
             return response()->json([

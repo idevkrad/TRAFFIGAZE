b8e40a8406a315bfd47793059dfd8321dcc8da4f
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-04-10 16:14:24 +0800

    we

--- app/Http/Controllers/Api/StatsController.php
+++ app/Http/Controllers/Api/StatsController.php
@@ -10,7 +10,50 @@ use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    public function index(Request $request){
+    public function tag(Request $request){
+        $series = [];
+        $names = [];
+
+        $query = Tag::query();
+        if($request->type != 'yesterday'){
+            $query->withCount('posts');
+        }else{
+            // $query->whereHas('posts',function ($query) {
+            //     $query->whereDate('created_at', Carbon::yesterday()); 
+            // });
+            $query->withCount([
+                'posts' => function (\Illuminate\Database\Eloquent\Builder $query) {
+                    $query->whereDate('created_at', Carbon::yesterday());
+                }
+            ]);
+        }
+        $tags = $query->orderBy('posts_count', 'desc')->get();
+        
+        if($request->type == 'yesterday'){
+            return response()->json([
+                'status' => true,
+                'message' => 'List fetched',
+                'lists' => $tags
+            ], 200);
+        }else{
+            foreach($tags as $tag){
+                $series[] = $tag->posts_count;
+                $names[] = $tag->name;
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'List fetched',
+                'lists' => $tags,
+                'data' => [
+                    'series' => $series,
+                    'labels' => $names
+                ]
+            ], 200);
+        }
+    }
+
+    public function barangay(Request $request){
         $series = [];
         $names = [];
 
--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,8 @@ Route::get('/lists', [App\Http\Controllers\Api\PostController::class, 'lists']);
 Route::get('/location', [App\Http\Controllers\Api\PostController::class, 'location']);
 Route::get('/notification', [App\Http\Controllers\Api\PostController::class, 'notification']);
 Route::get('/viewNoti', [App\Http\Controllers\Api\PostController::class, 'viewNoti']);
-Route::get('/stats', [App\Http\Controllers\Api\StatsController::class, 'index']);
+Route::get('/stats/tag', [App\Http\Controllers\Api\StatsController::class, 'tag']);
+Route::get('/stats/barangay', [App\Http\Controllers\Api\StatsController::class, 'barangay']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return new UserResource($request->user());

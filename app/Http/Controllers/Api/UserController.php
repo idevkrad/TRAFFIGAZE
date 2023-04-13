fa4fd7b6b8cb4d6eae31150b7f443c10ea54f642
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-04-13 08:59:28 +0800

    13

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
